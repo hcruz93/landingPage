6d2b0cebb18a44321e680e74ea66305ce272f655
Humberto <hum.alex93@gmail.com>
2025-04-25 13:11:13 -0600

    Create landig page

--- landingPageVue/src/main.js
+++ landingPageVue/src/main.js
@@ -1,4 +1,9 @@
-import './assets/main.css'
+import 'bootstrap/dist/css/bootstrap.min.css'; // Importing Bootstrap CSS
+import 'bootstrap-icons/font/bootstrap-icons.css'; // Importing Bootstrap Icons
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Importing Bootstrap JS
+// import SimpleLightbox from "simplelightbox";
+// import "simplelightbox/dist/simple-lightbox.min.css";  revisar no funciona
+import './assets/styles.css' // Importing custom styles
 
 import { createApp } from 'vue'
 import App from './App.vue'
@@ -9,3 +14,4 @@ const app = createApp(App)
 app.use(router)
 
 app.mount('#app')
+
--- landingPageVue/src/views/HomeView.vue
+++ landingPageVue/src/views/HomeView.vue
@@ -1,9 +1,20 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <Masthead />
+    <About />
+    <Services />
+    <Portfolio />
+    <CallAction />
+    <Contact />
   </main>
 </template>
+
+
+<script setup>
+import Masthead from '@/components/Masthead.vue';
+import About from '@/components/About.vue';
+import Services from '@/components/Services.vue';
+import Portfolio from '@/components/Portfolio.vue';
+import CallAction from '@/components/CallAction.vue';
+import Contact from '@/components/Contact.vue';
+</script>
