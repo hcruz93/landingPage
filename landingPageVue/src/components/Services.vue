01c55d76d7ca68be473ce335fc4a4979c0b28568
hcruz <hum.alex93@gmail>
2025-05-13 21:10:46 -0600

    Change to  array in Services.vue

--- landingPageVue/src/components/Services.vue
+++ landingPageVue/src/components/Services.vue
@@ -4,32 +4,11 @@
         <h2 class="text-center mt-0">At Your Service</h2>
         <hr class="divider" />
         <div class="row gx-4 gx-lg-5">
-            <div class="col-lg-3 col-md-6 text-center">
+            <div  v-for="(logo, index) in arrayLogos" :key="index" class="col-lg-3 col-md-6 text-center">
                 <div class="mt-5">
-                    <div class="mb-2"><i class="bi-gem fs-1 text-primary"></i></div>
-                    <h3 class="h4 mb-2">Sturdy Themes</h3>
-                    <p class="text-muted mb-0">Our themes are updated regularly to keep them bug free!</p>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6 text-center">
-                <div class="mt-5">
-                    <div class="mb-2"><i class="bi-laptop fs-1 text-primary"></i></div>
-                    <h3 class="h4 mb-2">Up to Date</h3>
-                    <p class="text-muted mb-0">All dependencies are kept current to keep things fresh.</p>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6 text-center">
-                <div class="mt-5">
-                    <div class="mb-2"><i class="bi-globe fs-1 text-primary"></i></div>
-                    <h3 class="h4 mb-2">Ready to Publish</h3>
-                    <p class="text-muted mb-0">You can use this design as is, or you can make changes!</p>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6 text-center">
-                <div class="mt-5">
-                    <div class="mb-2"><i class="bi-heart fs-1 text-primary"></i></div>
-                    <h3 class="h4 mb-2">Made with Love</h3>
-                    <p class="text-muted mb-0">Is it really open source if it's not made with love?</p>
+                    <div class="mb-2"><i :class="logo.logo"></i></div>
+                    <h3 class="h4 mb-2">{{ logo.title }}</h3>
+                    <p class="text-muted mb-0">{{ logo.description }}</p>
                 </div>
             </div>
         </div>
@@ -37,3 +16,15 @@
   </section>
 
 </template>
+
+
+<script setup>
+import { ref } from 'vue'
+
+const arrayLogos = [
+  {logo:'bi-gem fs-1 text-primary', title:'Sturdy Themes', description:'Our themes are updated regularly to keep them bug free!'},
+  {logo:'bi-laptop fs-1 text-primary', title:'Up to Date', description:'All dependencies are kept current to keep things fresh.'},    
+  {logo:'bi-globe fs-1 text-primary', title:'Ready to Publish', description:'You can use this design as is, or you can make changes!'},
+  {logo:'bi-heart fs-1 text-primary', title:'Made with Love', description:'Is it really open source if it\'s not made with love?'}
+]
+</script>
